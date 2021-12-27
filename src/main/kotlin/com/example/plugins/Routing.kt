7660f710ea54edfae2c43bf648309e1238b26a37
Javi <j.albenizlopez@gmail.com>
2021-12-27 10:30:32 +0000

    initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package com.example.plugins
+
+import com.example.routes.getAllHeroes
+import com.example.routes.root
+import io.ktor.routing.*
+import io.ktor.application.*
+import io.ktor.http.content.*
+
+
+fun Application.configureRouting() {
+
+    routing {
+        root()
+        getAllHeroes()
+
+        static("/images") {
+            resources("images")
+        }
+    }
+}
